967726f3953472cdb214640c18a0167a6d6b1425
Felipe Lopez <aflopezbec@gmail.com>
2023-09-21 21:46:51 -0500

    feat: add home component and set as default

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Dashboard from './dashboard/dashboard.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Score from './score/score.jsx'
-import AdminControl from './admin/admin.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './home/home';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Score />
+    <Home />
   </React.StrictMode>,
 )
 //<Dashboard />
