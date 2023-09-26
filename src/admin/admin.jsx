e094e538d57730215f067d85025bef35381e8891
Drkbrd <neltharion.18@gmail.com>
2023-09-26 14:43:56 -0500

    CRUD completo new

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -1,10 +1,9 @@
 import { useId, useState, useEffect } from 'react';
-import { createTeam, createStation, createUser, getAsyncTeams, updateTeam, deleteTeam } from '../firebase/teams_repository'
+import { createTeam, createStation, createUser, getAsyncTeams, updateTeam, deleteTeam, getAsyncUser, deleteUser, updateUser, updateStation, deleteStation, getAsyncStation, getAllStation } from '../firebase/teams_repository'
 
 function AdminControl() {
 
-
-
+    //Get date
     function getCurrentDateAndTime() {
         const dateTime = new Date();
         return dateTime.toLocaleString();
@@ -12,7 +11,7 @@ function AdminControl() {
     const dateDisplay = getCurrentDateAndTime();
     const dater = new Date(dateDisplay);
 
-
+    //Block update 
     function handleSubmit(e) {
         // Prevent the browser from reloading the page
         e.preventDefault();
@@ -26,18 +25,27 @@ function AdminControl() {
         //console.log(formJson);
     }
 
+    //TeamCRUD---------------------------------------------------------------------------------------------------------------------------------
+    // Form register team
+    const nameInputId = useId();
+    const colourInputId = useId();
+    const chantInputId = useId();
+    const teamInputId = useId();
+
+    //Create team
     function setTeamParam(key, value) {
         var newTeam = {};
         Object.assign(newTeam, team);
         newTeam[key] = value;
-        console.log(newTeam);
+        //console.log(newTeam);
         setTeam(newTeam);
-        console.log(newTeam);
+        //console.log(newTeam);
     }
 
+    //update team
     async function upsertTeam(team) {
         if (!team["create_at"]) {
-            team["create_at"] = dater
+            team["create_at"] = toString(dater)
         }
         if (!team["score"]) {
             team["score"] = 0
@@ -47,73 +55,113 @@ function AdminControl() {
         } else {
             await updateTeam(team)
         }
-        setTeam({ "id": "", "color": "#000000", "name": "", "chant": "", "amount": 0, "create_at": "" })
+        setTeam({ "color": "#000000", "name": "", "chant": "", "amount": 0, "create_at": dater })
 
     }
+    //Traer tabla teams y eitar
+    const [bringTable, setBringTable] = useState([]);
+    const [team, setTeam] = useState({});
+    useEffect(() => { getAsyncTeams(setBringTable) }, [])
+    //TeamCRUD---------------------------------------------------------------------------------------------------------------------------------
+
+    //UserCRUD---------------------------------------------------------------------------------------------------------------------------------
+    const userInputNmae = useId();
+    const userInputPassword = useId();
+    const userInputFalseAdmin = useId();
+    const userInputStation = useId();
+    const [selectedOption, setSelectedOption] = useState("{}");
+
+    var handleChange = (selectedOption) => {
+        console.log(selectedOption.currentTarget.value);
+        setSelectedOption(selectedOption.currentTarget.value);
+    };
 
 
-    // Form register team
-    const nameInputId = useId();
-    const colourInputId = useId();
-    const chantInputId = useId();
-    const teamInputId = useId();
+    //Create User
+    function setUserParam(key, value) {
+        var newUser = {};
+        Object.assign(newUser, user);
+        newUser[key] = value;
+        setUser(newUser);
+        console.log(newUser)
+    }
+    //update User
+    async function upsertUser(user) {
+        if (!user["id"]) {
+            await createUser(user)
+        } else {
+            await updateUser(user)
+        }
+        setUser({ "FK_station": "", "is_admin": "", "password": "", "userName": "" })
 
+    }
+    //Traer tabla teams y eitar
+    const [bringTableUsr, setTableUsr] = useState([]);
+    const [user, setUser] = useState({});
+    useEffect(() => { getAsyncUser(setTableUsr) }, [])
+
+
+    //UserCRUD---------------------------------------------------------------------------------------------------------------------------------
+
+    //StationCRUD---------------------------------------------------------------------------------------------------------------------------------
     // Form register Station
     const stationInputNmae = useId();
     const stationInputDescription = useId();
     const stationInputMax = useId();
     const stationInputMin = useId();
 
-    // Form register User
-    const userInputNmae = useId();
-    const userInputPassword = useId();
-    const userInputFalseAdmin = useId();
-    const userInputStation = useId();
+    //Create Station
+    function setStationParam(key, value) {
+        var newStation = {};
+        Object.assign(newStation, station);
+        newStation[key] = value;
+        setStation(newStation);
+        console.log(newStation)
+    }
+    //update User
+    async function upsertStation(station) {
+        if (!station["id"]) {
+            await createStation(station)
+        } else {
+            await updateStation(station)
+        }
+        setStation({ "name": "", "description": "", "maxVal": 0, "minValue": 0 })
 
-    //FOrm register Station
-    const [stationName, setStationName] = useState("");
-    const [stationDescr, setStationDescr] = useState("");
-    const [stationMAX, setStationMAX] = useState(0);
-    const [stationMin, setStationMin] = useState(0);
+    }
+    //Traer tabla teams y eitar
+    const [bringTableStt, setTableStt] = useState([]);
+    const [station, setStation] = useState({});
+    useEffect(() => { getAsyncStation(setTableStt) }, [])
+    //StationCRUD---------------------------------------------------------------------------------------------------------------------------------
 
-    //FOrm register Station
-    const [userName, setUserName] = useState("");
-    const [password, setPasword] = useState("");
-    const [isAdmin, setAdmin] = useState("");
-    const isTrueSet = (isAdmin === 'true');
-    const [stationFK, setStationFk] = useState("");
-
-    //Traer tabla y eitar
-    const [bringTable, setBringTable] = useState([]);
-    const [team, setTeam] = useState({});
-    useEffect(() => { getAsyncTeams(setBringTable) }, [])
     return (
         <>
+            {/*Table Teams------------------------------------------------------------------------------------------------------------------------------*/}
             <div className="container text-center">
                 <div className="row">
-                    <div className="col">ID</div>
+                    {/*<div className="col">ID</div>*/}
                     <div className="col">Name</div>
                     <div className="col">Chant</div>
                     <div className="col">Amount</div>
+                    <div className="col">Update</div>
                 </div>
             </div>
             <div className="container text-center">
                 {bringTable.map((e) => (
                     <div key={e.id} className="row">
-                        <div className="col">{e.id}</div>
+                        {/*<div className="col">{e.id}</div>*/}
                         <div className="col">{e.name}</div>
                         <div className="col">{e.chant}</div>
                         <div className="col">{e.amount}</div>
                         <div className="col">
-                            <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendTeam" onClick={() => setTeam(e)}>Edit</button>
-                        </div>
-                        <div className="col">
-                            <button type="submit" className="btn btn-outline-warning btn-lg" id="buttonSendTeam" onClick={() => deleteTeam(e)}>Delete</button>
+                            <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonUpdateTeam" onClick={() => setTeam(e)}>Edit</button>
+                            <button type="submit" className="btn btn-outline-warning btn-lg" id="buttonDeleteTeam" onClick={() => deleteTeam(e)}>Delete</button>
                         </div>
                     </div>
                 ))}
             </div>
-            {/*Form resgister team*/}
+            {/*Table Teams------------------------------------------------------------------------------------------------------------------------------*/}
+            {/*Form resgister team------------------------------------------------------------------------------------------------------------------------------*/}
             <form id="allForm" onSubmit={handleSubmit}>
                 <label htmlFor={colourInputId}>
                     Team's color:
@@ -138,63 +186,129 @@ function AdminControl() {
                 <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendTeam" onClick={() => upsertTeam(team)}>Send</button>
                 {/*createTeam(parseInt(teamTotal), teamChant, teamColour, dater, teamName, 0)*/}
             </form>
+            {/*Form resgister team------------------------------------------------------------------------------------------------------------------------------*/}
             <hr />
             <hr />
-            {/*Form resgister Station*/}
+            {/*Table station------------------------------------------------------------------------------------------------------------------------------*/}
+            <div className="container text-center">
+                <div className="row">
+                    {/*<div className="col">ID</div>*/}
+                    <div className="col">Name</div>
+                    <div className="col">Description</div>
+                    <div className="col">Max</div>
+                    <div className="col">min</div>
+                    <div className="col">Update</div>
+                </div>
+            </div>
+            <div className="container text-center">
+                {bringTableStt.map((e) => (
+                    <div key={e.id} className="row">
+                        {/*<div className="col">{e.id}</div>*/}
+                        <div className="col">{e.name}</div>
+                        <div className="col">{e.description}</div>
+                        <div className="col">{e.maxVal}</div>
+                        <div className="col">{e.minValue}</div>
+                        <div className="col">
+                            <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonUpdateSttn" onClick={() => setStation(e)}>Edit</button>
+                            <button type="submit" className="btn btn-outline-warning btn-lg" id="buttonDeleteSttn" onClick={() => deleteStation(e)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {/*Table Station------------------------------------------------------------------------------------------------------------------------------*/}
+            {/*Form resgister Station-----------------------------------------------------------------------------------------*/}
             <form onSubmit={handleSubmit}>
                 <label htmlFor={stationInputNmae}>
                     Station Name:
-                    <input id={stationInputNmae} name="SttnName" type="text" placeholder="Write the station's name" value={stationName} onChange={e => setStationName(e.target.value)}></input>
+                    <input id={stationInputNmae} name="SttnName" type="text" placeholder="Write the station's name" value={station.name} onChange={e => setStationParam("name", e.target.value)}></input>
                 </label>
                 <hr />
                 <label htmlFor={stationInputDescription}>
                     Description as step by step:
-                    <input id={stationInputDescription} name="DscptnSttn" type="text" placeholder="Write the station's description" value={stationDescr} onChange={e => setStationDescr(e.target.value)}></input>
+                    <input id={stationInputDescription} name="DscptnSttn" type="text" placeholder="Write the station's description" value={station.description} onChange={e => setStationParam("description", e.target.value)}></input>
                 </label>
                 <hr />
                 <label htmlFor={stationInputMax}>
                     Max points:
-                    <input id={stationInputMax} name="MxPnts" type="number" value={stationMAX} onChange={e => setStationMAX(e.target.value)}></input>
+                    <input id={stationInputMax} name="MxPnts" type="number" value={station.maxVal} onChange={e => setStationParam("maxVal", e.target.value)}></input>
                 </label>
                 <hr />
                 <label htmlFor={stationInputMin}>
                     Min points:
-                    <input id={stationInputMin} name="MnPnts" type="number" value={stationMin} onChange={e => setStationMin(e.target.value)}></input>
+                    <input id={stationInputMin} name="MnPnts" type="number" value={station.minValue} onChange={e => setStationParam("minValue", e.target.value)}></input>
                 </label>
                 <hr />
-                <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendStation" onClick={() => createStation(stationName, stationDescr, parseInt(stationMAX), parseInt(stationMin))}>Send</button>
+                <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendStation" onClick={() => upsertStation(station)}>Send</button>
             </form>
+            {/*Form resgister Station-----------------------------------------------------------------------------------------*/}
             <hr />
             <hr />
-            {/*Form resgister User*/}
+            {/*Table Users------------------------------------------------------------------------------------------------------------------------------*/}
+            <div className="container text-center">
+                <div className="row">
+                    {/*<div className="col">ID</div>*/}
+                    <div className="col">User name</div>
+                    <div className="col">Pasword</div>
+                    <div className="col">Is admin</div>
+                    <div className="col">Station</div>
+                    <div className="col">Update</div>
+                </div>
+            </div>
+            <div className="container text-center">
+                {bringTableUsr.map((f) => (
+                    <div key={f.id} className="row">
+                        {/*<div className="col">{e.id}</div>*/}
+                        <div className="col">{f.userName}</div>
+                        <div className="col">{f.password}</div>
+                        <div className="col">{f.is_admin}</div>
+                        <div className="col">{f.FK_station}</div>
+                        <div className="col">
+                            <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonUpdateUser" onClick={() => setUser(f)}>Edit</button>
+                            <button type="submit" className="btn btn-outline-warning btn-lg" id="buttonUpdateUser" onClick={() => deleteUser(f)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {/*Table Users------------------------------------------------------------------------------------------------------------------------------*/}
+            <hr />
+            <hr />
+            {/*Form resgister User-----------------------------------------------------------------------------------------------------------------------------------------*/}
             <form method='post' onSubmit={handleSubmit}>
                 <label htmlFor={userInputNmae}>
                     User name:
-                    <input id={userInputNmae} name="userName" type="text" placeholder="Write user's name" value={userName} onChange={e => setUserName(e.target.value)}></input>
+                    <input id={userInputNmae} name="userName" type="text" placeholder="Write user's name" value={user.userName} onChange={e => setUserParam("userName", e.target.value)}></input>
                 </label>
                 <hr />
                 <label htmlFor={userInputPassword}>
                     Pasword:
-                    <input id={userInputPassword} name="userPasword" type="password" value={password} onChange={e => setPasword(e.target.value)}></input>
+                    <input id={userInputPassword} name="userPasword" type="password" value={user.password} onChange={e => setUserParam("password", e.target.value)}></input>
                 </label>
                 <hr />
                 <label htmlFor={userInputFalseAdmin}>
                     Is admin:
-                    <input id="falseId" name="adminBoolean" type="radio" value="true" onChange={e => setAdmin(e.target.value)}></input>
+                    <input id="falseId" name="adminBoolean" type="radio" value={user.is_admin} onChange={e => setUserParam("is_admin", (e.target.value = true))}></input>
                     <label htmlFor="falseId">True</label><br></br>
-                    <input id="trueId" name="adminBoolean" type="radio" value="false" onChange={e => setAdmin(e.target.value)}></input>
+                    <input id="trueId" name="adminBoolean" type="radio" value={user.is_admin} onChange={e => setUserParam("is_admin", (e.target.value = false))}></input>
                     <label htmlFor="trueId">False</label><br></br>
                 </label>
                 <hr />
                 <label htmlFor={userInputStation}>
                     Station director:
-                    <input id={userInputStation} name="stationDirector" type="text" value={stationFK} onChange={e => setStationFk(e.target.value)}></input>
+                    <select className="form-select form-select-sm; bg-transparent" aria-label="Small select example" id="idTeamSelcted" onChange={handleChange}>
+                        <option defaultValue>Select a team</option>
+                        {teams.map((team) => {
+                            return <option key={team.id} value={JSON.stringify(team)} > {team.name}</option>
+                        })
+                        }
+                    </select>
+                    {/*<input id={userInputStation} name="stationDirector" type="text" value={user.FK_station} onChange={e => setUserParam("FK_station", e.target.value)}></input>*/}
                 </label>
                 <hr />
-                <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendUsear" onClick={() => createUser(userName, password, isTrueSet, stationFK)}>Send</button>
+                <button type="submit" className="btn btn-outline-primary btn-lg" id="buttonSendUsear" onClick={() => upsertUser(user)}>Send</button>
                 <hr />
                 <hr />
             </form>
+            {/*Form resgister User-----------------------------------------------------------------------------------------------------------------------------------------*/}
         </>
     );
 
--- src/firebase/teams_repository.jsx
+++ src/firebase/teams_repository.jsx
@@ -1,6 +1,8 @@
 import { db } from './firebase_config'
 import { collection, getDocs, query, onSnapshot, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
+
+//CRUD Team-------------------------------------------------------------------------------------------------------
 //Get all the teams, busca y devulve
 export async function getAllTeams(setTeams) {
     const querySnapshot = await getDocs(collection(db, "team"));
@@ -21,21 +23,6 @@ export async function getAsyncTeams(setTeams) {
             .map((doc) => ({ ...doc.data(), id: doc.id }));
         setTeams(newData)
     });
-}
-
-//Registrar escore del team
-export async function asignPoints(fkst, fkteam, timestam, totScore) {
-    try {
-        const docRef = await addDoc(collection(db, "score"), {
-            FK_station_score: fkst,
-            FK_team: fkteam,
-            creted_at: timestam,
-            score: totScore
-        });
-        console.log("the id for this item is ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
 }
 
 //Creación de un Team
@@ -64,33 +51,120 @@ export async function deleteTeam(team) {
         console.error("Error deleting team: ", e);
     }
 }
+//CRUD Team-------------------------------------------------------------------------------------------------------
 
+//CRUD Station-------------------------------------------------------------------------------------------------------
 //Creación de Station
-export async function createStation(nm, dscrpt, minVl, mxVl) {
+export async function createStation(station) {
     try {
-        const docRef = await addDoc(collection(db, "station"), {
-            name: nm,
-            description: dscrpt,
-            maxVal: mxVl,
-            minValue: minVl
-        });
-        console.log("Team written with ID: ", docRef.id);
+        const docRef = await addDoc(collection(db, "station"), station);
+        console.log("Station written with ID: ", docRef.id);
     } catch (e) {
-        console.error("Error adding team: ", e);
+        console.error("Error adding station: ", e);
+    }
+}
+//Get all the stations, busca y devulve 
+export async function getAllStation(setStation) {
+    const querySnapshot = await getDocs(collection(db, "station"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+    const newData = querySnapshot.docs
+        .map((doc) => ({ ...doc.data(), id: doc.id }));
+    setStation(newData)
+}
+
+//Get all the station - reactivo y cambia automatico 
+export async function getAsyncStation(setStation) {
+    const q = await query(collection(db, "station"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const newData = querySnapshot.docs
+            .map((doc) => ({ ...doc.data(), id: doc.id }));
+        setStation(newData)
+    });
+}
+//Update team  
+export async function updateStation(station) {
+    try {
+        const userRef = doc(db, "station", station.id);
+        await updateDoc(userRef, station);
+    } catch (e) {
+        console.error("Error adding station: ", e);
+    }
+}
+//Delete team
+export async function deleteStation(station) {
+    try {
+        await deleteDoc(doc(db, "station", station.id));
+    } catch (e) {
+        console.error("Error deleting station: ", e);
+    }
+}
+//CRUD Station-------------------------------------------------------------------------------------------------------
+
+//CRUD User-------------------------------------------------------------------------------------------------------
+//Creación de users
+export async function createUser(user) {
+    try {
+        const docRef = await addDoc(collection(db, "user"), user);
+        console.log("User written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding User: ", e);
     }
 }
 
-//Creación de users
-export async function createUser(usrNme, psswrd, sdmn, fkSttn) {
+//Get all the Users, busca y devulve
+export async function getAllUsers(setUser) {
+    const querySnapshot = await getDocs(collection(db, "user"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+    const newData = querySnapshot.docs
+        .map((doc) => ({ ...doc.data(), id: doc.id }));
+    setUser(newData)
+}
+
+//Get all the users - reactivo y cambia automatico
+export async function getAsyncUser(setUser) {
+    const q = await query(collection(db, "user"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const newData = querySnapshot.docs
+            .map((doc) => ({ ...doc.data(), id: doc.id }));
+        setUser(newData)
+    });
+}
+//Update team
+export async function updateUser(user) {
     try {
-        const docRef = await addDoc(collection(db, "user"), {
-            userName: usrNme,
-            password: psswrd,
-            is_admin: sdmn,
-            FK_station: fkSttn
-        });
-        console.log("Team written with ID: ", docRef.id);
+        const userRef = doc(db, "user", user.id);
+        await updateDoc(userRef, user);
     } catch (e) {
-        console.error("Error adding team: ", e);
+        console.error("Error adding user: ", e);
+    }
+}
+//Delete team
+export async function deleteUser(user) {
+    try {
+        await deleteDoc(doc(db, "user", user.id));
+    } catch (e) {
+        console.error("Error deleting user: ", e);
+    }
+}
+//CRUD User-------------------------------------------------------------------------------------------------------
+
+//Registrar escore del team
+export async function asignPoints(fkst, fkteam, timestam, totScore) {
+    try {
+        const docRef = await addDoc(collection(db, "score"), {
+            FK_station_score: fkst,
+            FK_team: fkteam,
+            creted_at: timestam,
+            score: totScore
+        });
+        console.log("the id for this item is ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
     }
 }
